2cccd3ed5d18b5c2e6760024dd491c3d4895b463
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-03-13 17:32:02 -0500

    update for team_11.r

--- R/team_11.R
+++ R/team_11.R
@@ -1,19 +1,16 @@
-# This function extracts data from a shapefile with 2 levels in spf$geometry
-extract_data_L2 <- function(spf) {
-  # Reads in the shapefile and extracts data from nested lists, finally
-  # recording them into one data frame with columns: long, lat, group and
-  # order.
-  #
-  # Args:
-  #   spf:    shapefile of a country
-  #
-  # Output:
-  #   df:     data frame
+#'@name team_11
+#'@title team_11's function for 2-level list extraction
+#'@author Lab 2 team 11 from STAT 585 Spring 2019
+#'@import "ggplot2", "sf", "ggspatial","maps","maptools","rgeos","purrr","tidyverse"
+#'@seealso purr::flatten
+#'@return a small shape file
+#'@description This function extracts data from a shapefile with 2 levels in spf$geometry
+
+
+team_11 <- function(spf) {
   map_depth(.x = spf$geometry, 2, .f = c(1)) %>% flatten %>%
     map_dfr(data.frame, .id = "group") -> df
-  # rename columns
   colnames(df) <- c("group", "long", "lat")
-  # add new variable = "order"
   df$order <- seq(from = 1, to = nrow(df), by = 1)
   return(df)
 }
