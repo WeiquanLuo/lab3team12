f88dfab503c72a2662bae51b4c52a3e9593e4dc6
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-03-13 17:51:46 -0500

    update team_11

--- R/team_11.R
+++ R/team_11.R
@@ -1,11 +1,12 @@
 #'@name team_11
 #'@title team_11's function for 2-level list extraction
 #'@author Lab 2 team 11 from STAT 585 Spring 2019
-#'@import "ggplot2", "sf", "ggspatial","maps","maptools","rgeos","purrr","tidyverse"
+#'@import ggplot2, sf, ggspatial, maps, maptools, rgeos, purrr, tidyverse
 #'@seealso purr::flatten
 #'@return a small shape file
 #'@description This function extracts data from a shapefile with 2 levels in spf$geometry
-
+#' @export
+#' @importFrom purrr flatten
 
 team_11 <- function(spf) {
   map_depth(.x = spf$geometry, 2, .f = c(1)) %>% flatten %>%
