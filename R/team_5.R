608db83e789cc505cb2712c2fd74e69818c872b8
kmcclernon <kelliem@iastate.edu>
2019-03-23 12:32:16 -0500

    add tests for team_5

--- R/team_5.R
+++ R/team_5.R
@@ -1,16 +1,5 @@
-#' Team 5
-#'
-#' Converts the geometry section of a shape file to latitude-longitude format
-#' @export
-#' @param file Shape file path, extension .shp. Must contain a geometry column.
-#' @param tolerance Tolerance level for thinning shape file. A percentage between 0 and 1.
-#' @return The structure of the original file with the geometry column converted to long-lat format.
-#'
-#' @import purrr
-#' @import assertthat
-
-library(sf)
-
+# helper function
+# takes matrix and converts to dataframe with order of rows preserved in order column
 Mat2Df <- function(Mat){
   long <- Mat[,1]
   lat <- Mat[,2]
@@ -20,23 +9,52 @@ Mat2Df <- function(Mat){
   return(df)
 }
 
+# helper function
+# takes multipoly geometry list and returns data frame
 newgeo <- function(multpoly){
-  lst <- flatten(multpoly) %>% purrr::map(.f = Mat2Df) %>%
-    bind_rows(.id = "group")
-  newdf <- lst %>% select(-group, -order, everything())
-  newdf$group <- as.factor(newdf$group)
+  lst <- purrr::flatten(multpoly) %>% purrr::map(.f = Mat2Df) %>%
+    dplyr::bind_rows(.id = "subgroup")
+  newdf <- lst %>% dplyr::select(-subgroup, -order, dplyr::everything())
+  newdf$subgroup <- as.factor(newdf$subgroup)
   return(newdf)
 }
+
+#' Team 5
+#'
+#' Converts the geometry section of a shape file to latitude-longitude format
+#' @name team_5
+#' @author Kellie McClernon
+#' @export team_5
+#' @param file Shape file path, extension .shp. Must contain a geometry column.
+#' @param tolerance Tolerance level for thinning shape file. A percentage between 0 and 1.
+#' @return a data frame with latitude-longitude and additional geography information
+#'
+#' @import assertthat
+#' @import dplyr
+#' @import purrr
+#' @importFrom maptools thinnedSpatialPoly
+#' @importFrom sf read_sf st_as_sf
 
 team_5 <- function(file, tolerance = 0.05){
   assertthat::assert_that(assertthat::has_extension(file, "shp"))
   sh_orig <- sf::read_sf(as.character(file))
-  shp_thin <- maptools::thinnedSpatialPoly(as(file, "Spatial"),
+  shp_thin <- maptools::thinnedSpatialPoly(as(sh_orig, "Spatial"),
                                tolerance = tolerance, minarea = 0.001,
                                topologyPreserve = TRUE)
   sf_thin <- sf::st_as_sf(shp_thin)
 
-  new_sf <- sf_thin %>% transmute(data = geometry %>%
+  # returns a list of data frames with the geometry information
+  assertthat::assert_that(assertthat::has_name(sf_thin, "geometry"))
+  new_sf <- sf_thin %>% dplyr::transmute(data = geometry %>%
                                     purrr::map(.f = function(x){newgeo(x)}))
-  return(new_sf)
+
+  # converting to data frame with geographic information
+  geom_data <- new_sf$data %>% dplyr::bind_rows(.id = "group") %>%
+    dplyr::select(-group, -subgroup, -order, dplyr::everything())
+  geom_data$group <- as.factor(geom_data$group)
+  rest <- dplyr::select(as.data.frame(new_df), -which(names(new_df) == "geometry"))
+  rest <- dplyr::add_rownames(rest, var = "group")
+  rest$group <- as.factor(rest$group)
+  df.final <- dplyr::right_join(geom_data, rest, by = "group")
+  return(df.final)
 }
--- tests/testthat/test-fun-team_5.R
+++ tests/testthat/test-fun-team_5.R
@@ -11,16 +11,16 @@ test_that("input file is correct", {
   expect_that(team_5(file_bad, tolerance = 0.1), throws_error())
 
   # has geometry column
-  shp_big <- sf::read_sf(file_good)
-  shp_sp <- maptools::thinnedSpatialPoly(sf::as(shp_big, "Spatial"),
-                                           tolerance = 0.1, minarea = 0.001,
-                                           topologyPreserve = TRUE)
-  shp_thin <- sf::st_as_sf(shp_sp)
+  shp_good <- lab3team12:::aus_shp
+  shp_thin_bad <- dplyr::select(as.data.frame(shp_good), -which(names(shp_good) == "geometry"))
+  expect_that(has_name(shp_good, "geometry"), is_true())
+  expect_that(has_name(shp_thin_bad, "geometry"), is_false())
 
 })
 
 test_that("sub-function Mat2Df makes the correct data frame", {
-  test <- oz$geometry[[1]][[1]][[1]] ## need to create properly!!
+  aus_shp <- lab3team12:::aus_shp
+  test <- aus_shp$geometry[[1]][[1]][[1]]
 
   test_output <- Mat2Df(test)
   df_len <- length(test_output$order)
@@ -31,23 +31,32 @@ test_that("sub-function Mat2Df makes the correct data frame", {
 })
 
 test_that("new geometry function has correct output", {
-  test2 <- oz$geometry[[1]]
+  aus_shp <- lab3team12:::aus_shp
+  test2 <- aus_shp$geometry[[1]]
   sub_list <- test2[[1]][[1]]
   sub_len <- length(sub_list[,1])
   t2_output <- newgeo(test2)
 
   # check that group is correct
-  expect_that(t2_output$group[1], equals(1))
-  expect_that(t2_output$group[sub_len], matches("1"))
-  expect_that(t2_output$group[1+sub_len], matches("2"))
+  char_sg <- as.character(t2_output$subgroup)
+  expect_that(char_sg[1], is_identical_to("1"))
+  expect_that(char_sg[sub_len], is_identical_to("1"))
+  expect_that(char_sg[1+sub_len], is_identical_to("2"))
 
   # check format of output
-  expect_that(names(t2_output), matches(c("long", "lat", "group", "order")))
+  expect_that(names(t2_output), is_equivalent_to(c("long", "lat", "subgroup", "order")))
   expect_that(is.data.frame(t2_output), is_true())
 })
 
-test_that("final team_5 function has new geometry and other columns", {
-  file_oz <- "data/AUS_1/gadm36_AUS_1.shp"
-
-  team_5(file_oz, tolerance = 0.1)
-})
+#test_that("final team_5 function has new geometry and other columns", {
+#  file_oz <- "data/AUS_1/gadm36_AUS_1.shp"
+#
+#  f_output <- team_5(file_oz, tolerance = 0.1)
+#  all_d <- make_shp(file_oz, tolerance = 0.1)
+#  other <- dplyr::select(as.data.frame(all_d), -which(names(all_d) == "geometry"))
+#
+#  expect_that(is.data.frame(f_output), is_true())
+#  expect_that(names(f_output)[1:5], matches(c("long", "lat", "group", "subgroup", "order")))
+#  expect_that(length(names(f_output)), equals(length(names(other))+5))
+#})
+# Note: I cannot figure out how to store a .shp in the package to test this part of the function :(
