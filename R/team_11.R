a21ad578e4836a84a1d1b1f873a47eb453814b3d
Hao Wang <glambugle@gmail.com>
2019-03-23 22:31:04 -0500

    updated

--- R/team_11.R
+++ R/team_11.R
@@ -1,19 +1,20 @@
-#'Converts the geometry section of a shape file to latitude-longitude format
 #'@name team_11
 #'@title team_11's function for 2-level list extraction
-#'@author Lab 2 team 11 from STAT 585 Spring 2019
-#'@export
 #'@description This function extracts data from a shapefile with 2 levels in file$geometry
-#'@importFrom sf read_sf st_as_sf
-#'@importFrom maptools thinnedSpatialPoly
-#'@importFrom purrr map_depth flatten map_dfr
+#'@usage team_11(tile, tolerance)
 #'@param file path of a geometry file, extension should be .shp.
 #'@param tolerance Tolerance level for thinning shape file. A percentage between 0 and 1.
+#'@details Converts the geometry section of a shape file to latitude-longitude format
 #'@return a small shape file
+#'@author Lab 2 team 11 from STAT 585 Spring 2019
+#'@seealso team_11, team_5
 #'@examples
 #'dsn="data/gadm36_AUS_shp/gadm36_AUS_1.shp"
 #'tmp=team_11(file = dsn)
-#'@seealso purrr::map_depth, purrr::map_dfr
+#'@export
+#'@importFrom sf read_sf st_as_sf
+#'@importFrom maptools thinnedSpatialPoly
+#'@importFrom purrr map_depth flatten map_dfr
 
 team_11 <- function(file, tolerance = 0.1) {
 
